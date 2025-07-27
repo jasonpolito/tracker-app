da75e9160fdd4a53033d64e158b2a9bada3ca119
Jason Polito <jasonpolito@gmail.com>
2025-07-27 19:50:48 -0400

    duplication functionality, UI improvements

--- assets/js/tracker.js
+++ assets/js/tracker.js
@@ -1,13 +1,18 @@
-const database = new LDBX("tracker", true, true);
-let tracker = database.where("name", "tracker").first();
+const SETTINGS = {
+  editing: null,
+  data: [],
+};
+
+let tracker = window.localStorage.getItem("appData");
 
 if (!tracker) {
-  database.save({
-    name: "tracker",
-    data: [{}],
-  });
-  tracker = database.where("name", "tracker").first();
+  window.localStorage.setItem("appData", JSON.stringify(SETTINGS));
 }
+
+tracker = window.localStorage.getItem("appData");
+tracker = JSON.parse(tracker);
+
+SETTINGS.tracker = tracker;
 
 function uuidv4() {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
@@ -21,13 +26,13 @@ const app = new Vue({
   el: "#app",
   data() {
     return {
-      tracker,
+      ...SETTINGS,
     };
   },
   watch: {
     tracker: {
       handler(newValue, oldValue) {
-        database.update(this.tracker);
+        window.localStorage.setItem("appData", JSON.stringify(this.tracker));
       },
       deep: true,
     },
@@ -35,17 +40,31 @@ const app = new Vue({
   methods: {
     createDataPoint() {
       const dataPoint = {
-        name: "New Data Point",
+        name: `New Data Point ${this.tracker.data.length + 1}`,
         value: 0,
         id: uuidv4(),
       };
       this.tracker.data.push(dataPoint);
+    },
+    duplicateDataPoint(id) {
+      const item = this.tracker.data.find((item) => item.id === id);
+      if (item) {
+        const newItem = { ...item, name: `${item.name} (Copy)`, id: uuidv4() };
+        // insert new item after the original
+        const index = this.tracker.data.findIndex((i) => i.id === id);
+        if (index !== -1) {
+          this.tracker.data.splice(index + 1, 0, newItem);
+        } else {
+          this.tracker.data.push(newItem);
+        }
+        this.editing = newItem.id;
+      }
     },
     deleteDataPoint(id) {
       this.tracker.data = this.tracker.data.filter((item) => item.id !== id);
     },
   },
   beforeMount() {
-    this.tracker = tracker;
+    this.$data = SETTINGS;
   },
 });
